f8ae9556f19923f312a4812955a0ca54737d87b4
UG <ug00257793@gmail.com>
2022-10-24 16:37:23 +0800

    update player.py

--- Player.py
+++ Player.py
@@ -1,3 +1,5 @@
+from MahjongKit.MahjongKit import Tile, Meld, Partition, WinWaitCal
+
 class Player:
     wind = 0
     seat = 0
@@ -42,3 +44,45 @@ class Player:
 
     def action(self):
         return [-1]
+
+    def get_shantin(self):
+        bonus_chr = filter(lambda f:f in [self.wind+27, self.seat+27 ,31, 32, 33], self.tiles)
+        return Partition.shantin_multiple_forms(self.tiles, self.open_melds, bonus_chr)
+    
+    
+    # return [int:how_many_ways_to_chi, [ways of chi]]
+    def can_chi(self, tile, from_player):
+        if((self.seat - from_player)%4 != 1):
+            return [0, []]
+        else:
+            chi_lst = []
+            for parts in Partition.partition(self.tiles):
+                for part in parts:
+                    if(len(part)==2 and part[0]!=part[1]):
+                        tmp = [part+tile].sort 
+                        if((tmp in Tile.index_to_chow) and (tmp not in chi_lst)):
+                            chi_lst.append(tmp)
+            return [len(chi_lst), chi_lst]
+
+    # return [int:0/1, [pon]]
+    def can_pon(self, tile):
+        if(self.tiles.count(tile) < 2):
+            return [0, []]
+        else:
+            return [1, [tile, tile, tile]]
+
+    # return [int:0/1, [minkan]]
+    def can_minkan(self, tile):
+        if(self.tiles.count(tile) != 3):
+            return [0, []]
+        else:
+            return [1, [tile, tile, tile, tile]]
+
+    def can_ankan(self, tile):
+        pass
+
+    def can_win(self):
+        pass
+
+    def can_riichi(slef):
+        pass
